f998f68aa8105fbd5a6787401729f5657f31a67b
AhmedGouda <AhmedGouda@DESKTOP-5ORO327>
2024-11-17 16:30:30 +0200

    Adding Domain Core

---
+++ CleanArch.Domain.Core/Commands/Command.cs
@@ -0,0 +1,13 @@
+﻿using CleanArch.Domain.Core.Events;
+
+namespace CleanArch.Domain.Core.Commands
+{
+    public class Command : Message
+    {
+        public DateTime TimeStamp { get; protected set; }
+        public Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
